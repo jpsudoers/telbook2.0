254ff644d1ae93c562ddb8bf5e8492aac4b601c9
Juan Pablo Silva <juanpablo@sudoers.cl>
2024-03-05 21:58:29 -0300

    Fix error asistencia Retirados

--- src/components/curso/LandingAttendance.js
+++ src/components/curso/LandingAttendance.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 import StudentsContext from "@/context/students/Students.context";
 import {useRouter} from "next/router";
-import {getStudentsByGrade} from "@/utils/student";
+import {getStudentsByGrade, getStudentsByStatusActive} from "@/utils/student";
 import Current from "@/components/curso/asistencia/Current";
 import UserContext from "@/context/user/User.context";
 
@@ -17,9 +17,10 @@ const LandingAttendance = () => {
     const router = useRouter();
     const {grade} = router.query;
 // JPS se saca el filtro que no muestra asistencia
-    const filterStudents = getStudentsByGrade(students, grade)
+    const studentsByGrade = getStudentsByGrade(students, grade)
+    const studentsByStatus = getStudentsByStatusActive(studentsByGrade)
     return (
-        <Current students={filterStudents} user={user} grade={grade}/>
+        <Current students={studentsByStatus} user={user} grade={grade}/>
     );
 };
 
--- src/utils/student.js
+++ src/utils/student.js
@@ -1,4 +1,10 @@
-export const getStudentsByGrade = (students, grade) => students.filter(student => student.grade === grade.toUpperCase())
+export const getStudentsByGrade = (students, grade) => students.filter(student => {
+    return student.grade === grade.toUpperCase()
+})
+
+export const getStudentsByStatusActive = (students) => students.filter(student => {
+    return student.state === "Activo"
+})
 
 export function uuidv4() {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
